1bdee072b0ecb78243a9b43f911ae1350c8f8ccd
Lucas Devitto <lucasdevitto@hotmail.com>
2022-03-08 23:10:38 -0300

    Rotas implementadas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
+import Navbar from "components/NavBar";
 
 function App() {
-  return <NavBar />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
